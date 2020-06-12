4e3d7e8e647094ca9fff9582e40f9d6e814f2cbb
Facundo Batista <facundo@taniquetil.com.ar>
2020-06-12 10:39:57 -0300

    Refactored all the message sending to the user, including storing everything in a file.

--- charmcraft/logsetup.py
+++ charmcraft/logsetup.py
@@ -17,23 +17,90 @@
 """Set up logging."""
 
 import logging
+import os
+import tempfile
 
 
-_MODES = {
-    'quiet': (logging.WARNING, "%(message)s"),
-    'normal': (logging.INFO, "%(message)s"),
-    'verbose': (logging.DEBUG, "%(asctime)s  %(name)-18s %(levelname)-8s %(message)s"),
-}
+FORMATTER_SIMPLE = "%(message)s"
+FORMATTER_DETAILED = "%(asctime)s  %(name)-30s %(levelname)-8s %(message)s"
 
 _logger = logging.getLogger('charmcraft')
 _logger.setLevel(logging.DEBUG)
 
-_stderr_handler = logging.StreamHandler()
-_logger.addHandler(_stderr_handler)
+
+class _MessageHandler:
+    """Handle all the messages to the user.
+
+    This class deals with several combination of the following dimensions:
+
+    - the mode: quiet, normal or verbose
+    - the output: sometimes, some messages, to the terminal; always to the file
+    - the execution result: what happens if succeeded, raise a controlled error, or crashed
+    """
+
+    _modes = {
+        'quiet': (logging.WARNING, FORMATTER_SIMPLE),
+        'normal': (logging.INFO, FORMATTER_SIMPLE),
+        'verbose': (logging.DEBUG, FORMATTER_DETAILED),
+    }
+
+    def __init__(self):
+        self._stderr_handler = logging.StreamHandler()
+        _logger.addHandler(self._stderr_handler)
+
+        # autoset modes constants for simpler interface
+        for k in self._modes:
+            setattr(self, k.upper(), k)
+
+    def init(self, initial_mode):
+        """Initialize internal structures; this must be done before start logging."""
+        self._set_filehandler()
+        self.set_mode(initial_mode)
+
+    def set_mode(self, mode):
+        """Set logging in different modes."""
+        self.mode = mode
+        level, format_string = self._modes[mode]
+        self._stderr_handler.setFormatter(logging.Formatter(format_string))
+        self._stderr_handler.setLevel(level)
+
+    def _set_filehandler(self):
+        """Set the file handler to log everything to the temp file."""
+        _, self._log_filepath = tempfile.mkstemp(prefix='charmcraft-log-')
+
+        file_handler = logging.FileHandler(self._log_filepath)
+        file_handler.setFormatter(logging.Formatter(FORMATTER_DETAILED))
+        file_handler.setLevel(0)  # log eeeeeverything
+        _logger.addHandler(file_handler)
+
+        # a logger for only the file
+        self._file_logger = logging.getLogger('charmcraft.guard')
+        self._file_logger.propagate = False
+        self._file_logger.addHandler(file_handler)
+
+    def ended_ok(self):
+        """Cleanup after successful execution."""
+        os.unlink(self._log_filepath)
+
+    def ended_cmderror(self, err):
+        """Report the (expected) problem and logfile location."""
+        msg = "{} (full execution logs in {})".format(err, self._log_filepath)
+        _logger.error(msg)
+
+    def ended_crash(self, err):
+        """Report the internal error and logfile location.
+
+        Show just the error to the user, but send the whole traceback to the log file.
+        """
+        msg = "charmcraft internal error! {}: {} (full execution logs in {})".format(
+            err.__class__.__name__, err, self._log_filepath)
+        if self.mode == self.VERBOSE:
+            # both to screen and file!
+            _logger.exception(msg)
+        else:
+            # the error to screen and file, plus the traceback to the file
+            _logger.error(msg)
+            self._file_logger.exception('')
 
 
-def configure(mode):
-    """Set logging in different modes."""
-    level, format_string = _MODES[mode]
-    _stderr_handler.setFormatter(logging.Formatter(format_string))
-    _stderr_handler.setLevel(level)
+message_handler = _MessageHandler()
---
+++ tests/test_logsetup.py
@@ -0,0 +1,188 @@
+# Copyright 2020 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For further info, check https://github.com/canonical/charmcraft
+
+import logging
+import os
+from unittest.mock import patch
+
+from charmcraft.logsetup import _MessageHandler
+from charmcraft.cmdbase import CommandError
+
+import pytest
+
+
+@pytest.fixture
+def create_message_handler(tmp_path):
+    """Helper to create a message handler.
+
+    Always in a temp directory, maybe with patched modes.
+    """
+    temp_log_file = tmp_path / 'test.log'
+    patchers = []
+
+    def factory(modes=None):
+        p = patch('tempfile.mkstemp', lambda prefix: ('fd', str(temp_log_file)))
+        p.start()
+        patchers.append(p)
+
+        if modes is not None:
+            p = patch.object(_MessageHandler, '_modes', modes)
+            p.start()
+            patchers.append(p)
+
+        return _MessageHandler()
+
+    yield factory
+
+    # cleanup
+    for p in patchers:
+        p.stop()
+
+
+# --- Tests for the MessageHandler
+
+def test_mode_setting_init(create_message_handler):
+    """The internal mode is set on init and logger properly changed."""
+    test_level = 123
+    test_format = "this is a format"
+    mh = create_message_handler({'foo': (test_level, test_format)})
+    mh.init(mh.FOO)
+
+    assert mh.mode == mh.FOO
+    assert mh._stderr_handler.formatter._fmt == test_format
+    assert mh._stderr_handler.level == test_level
+
+
+def test_mode_setting_changed(create_message_handler):
+    """The internal mode can be set later and logger properly changed."""
+    test_level = 123
+    test_format = "this is a format"
+    mh = create_message_handler({'foo': (0, ''), 'bar': (test_level, test_format)})
+    mh.init(mh.FOO)
+    mh.set_mode(mh.BAR)
+
+    assert mh.mode == mh.BAR
+    assert mh._stderr_handler.formatter._fmt == test_format
+    assert mh._stderr_handler.level == test_level
+
+
+def test_logfile_all_stored(create_message_handler):
+    """All logging levels are dumped to the logfile."""
+    mh = create_message_handler()
+    mh.init(mh.NORMAL)
+
+    logger = logging.getLogger("charmcraft.test")
+    logger.debug("test debug")
+    logger.info("test info")
+    logger.warning("test warning")
+    logger.error("test error")
+
+    with open(mh._log_filepath, 'rt') as fh:
+        logcontent = fh.readlines()
+
+    assert 'test debug' in logcontent[0]
+    assert 'test info' in logcontent[1]
+    assert 'test warning' in logcontent[2]
+    assert 'test error' in logcontent[3]
+
+
+def test_ended_success(caplog, create_message_handler):
+    """Reports nothing, removes log file."""
+    caplog.set_level(logging.DEBUG, logger="charmcraft")
+
+    mh = create_message_handler()
+    mh.init(mh.NORMAL)
+    mh.ended_ok()
+
+    # file is removed, no particular message emitted
+    assert not os.path.exists(mh._log_filepath)
+    assert not caplog.records
+
+
+def test_ended_commanderror(caplog, create_message_handler):
+    """Reports just the message, including the log file (not removed)."""
+    caplog.set_level(logging.DEBUG, logger="charmcraft")
+
+    mh = create_message_handler()
+    mh.init(mh.NORMAL)
+    mh.ended_cmderror(CommandError("test controlled error"))
+
+    expected_msg = "test controlled error (full execution logs in {})".format(mh._log_filepath)
+
+    # file is present, and it has the error
+    with open(mh._log_filepath, 'rt', encoding='ascii') as fh:
+        log_content = fh.read()
+    assert 'ERROR' in log_content
+    assert expected_msg in log_content
+
+    # also it shown the error to the user
+    assert [expected_msg] == [rec.message for rec in caplog.records]
+
+
+def test_ended_crash_while_normal(caplog, create_message_handler):
+    """Reports the internal crash, including the log file (not removed)."""
+    caplog.set_level(logging.DEBUG, logger="charmcraft")
+
+    mh = create_message_handler()
+    mh.init(mh.NORMAL)
+    try:
+        raise ValueError("crazy crash")
+    except Exception as err:
+        mh.ended_crash(err)  # needs to have an exception "active"
+
+    expected_msg = (
+        "charmcraft internal error! ValueError: crazy crash (full execution logs in {})".format(
+            mh._log_filepath))
+
+    # file is present, and it has the error and the traceback
+    with open(mh._log_filepath, 'rt', encoding='ascii') as fh:
+        log_content = fh.read()
+    assert 'ERROR' in log_content
+    assert expected_msg in log_content
+    assert 'Traceback' in log_content
+
+    # also it shown ONLY the error to the user
+    (record,) = caplog.records
+    assert record.message == expected_msg
+    assert record.exc_info is None
+
+
+def test_ended_crash_while_verbose(caplog, create_message_handler):
+    """Reports the internal crash, including the log file (not removed)."""
+    caplog.set_level(logging.DEBUG, logger="charmcraft")
+
+    mh = create_message_handler()
+    mh.init(mh.VERBOSE)
+    try:
+        raise ValueError("crazy crash")
+    except Exception as err:
+        mh.ended_crash(err)  # needs to have an exception "active"
+
+    expected_msg = (
+        "charmcraft internal error! ValueError: crazy crash (full execution logs in {})".format(
+            mh._log_filepath))
+
+    # file is present, and it has the error and the traceback
+    with open(mh._log_filepath, 'rt', encoding='ascii') as fh:
+        log_content = fh.read()
+    assert 'ERROR' in log_content
+    assert expected_msg in log_content
+    assert 'Traceback' in log_content
+
+    # also it shown the error to the user AND also the traceback
+    (record,) = caplog.records
+    assert record.message == expected_msg
+    assert record.exc_info[0] == ValueError

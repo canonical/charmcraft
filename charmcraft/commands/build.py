aabf060efba609652e0bb61eed83a4deb4a7f982
John R. Lenton <jlenton@gmail.com>
2020-08-19 14:36:19 +0100

    cope with bionic's slightly broken pip3 command
    This makes 'charmcraft build' work even when running using the pip
    shipped in bionic, which is a bit "off".
    
    fixes: #102

--- charmcraft/commands/build.py
+++ charmcraft/commands/build.py
@@ -51,6 +51,15 @@ MANDATORY_HOOK_NAMES = {'install', 'start', 'upgrade-charm'}
 HOOKS_DIR = 'hooks'
 
 
+def _pip_is_bionic():
+    try:
+        from pip.commands.install import InstallCommand
+        return InstallCommand().cmd_opts.get_option('--system') is not None
+    except (ImportError, AttributeError, TypeError):
+        # probably not the bionic pip version then
+        return False
+
+
 def polite_exec(cmd):
     """Execute a command, only showing output if error."""
     logger.debug("Running external command %s", cmd)
@@ -223,6 +232,9 @@ class Builder:
                 'pip3', 'install',  # base command
                 '--target={}'.format(venvpath),  # put all the resulting files in that specific dir
             ]
+            if _pip_is_bionic():
+                logger.debug("adding --system to work around broken bionic pip3")
+                cmd.append("--system")
             for reqspath in self.requirement_paths:
                 cmd.append('--requirement={}'.format(reqspath))  # the dependencies file(s)
             retcode = polite_exec(cmd)

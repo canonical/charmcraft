0f6d2c9a0165169a512283b0f9150c8a244cfd03
John R. Lenton <jlenton@gmail.com>
2020-09-21 13:47:58 +0100

    use pytest.approx to compare times

--- tests/commands/test_build.py
+++ tests/commands/test_build.py
@@ -612,9 +612,8 @@ def _test_build_generics_tree(tmp_path, caplog, *, expect_hardlinks):
             assert filecmp.cmp(str(p1), str(p2), shallow=False)
             assert p1.stat().st_mode == p2.stat().st_mode
             assert p1.stat().st_size == p2.stat().st_size
-            # checking st_atime seems to be a bit racy
-            # assert p1.stat().st_atime == p2.stat().st_atime
-            assert p1.stat().st_mtime == p2.stat().st_mtime
+            assert pytest.approx(p1.stat().st_atime, p2.stat().st_atime)
+            assert pytest.approx(p1.stat().st_mtime, p2.stat().st_mtime)
 
 
 def test_build_generics_tree(tmp_path, caplog):

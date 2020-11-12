1033dc922de9afa045857c538ceabe477b8bc5bd
Facundo Batista <facundo@taniquetil.com.ar>
2020-11-12 15:22:59 -0300

    Support for publishing libraries.

--- charmcraft/main.py
+++ charmcraft/main.py
@@ -89,7 +89,7 @@ COMMAND_GROUPS = [
         # release process, and show status
         store.ReleaseCommand, store.StatusCommand,
         # libraries support
-        store.CreateLibCommand,
+        store.CreateLibCommand, store.PublishLibCommand,
     ]),
 ]
 

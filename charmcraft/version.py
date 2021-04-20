6caba8584ba66726312be21107766e122f835e07
Facundo Batista <facundo@taniquetil.com.ar>
2021-04-20 14:22:36 -0300

    Release 0.10.0

--- charmcraft/version.py
+++ charmcraft/version.py
@@ -19,7 +19,7 @@ from pathlib import Path
 
 __all__ = ('version',)
 
-_FALLBACK = '0.9.0'  # this gets bumped after release
+_FALLBACK = '0.10.0'  # this gets bumped after release
 
 
 def _get_version():

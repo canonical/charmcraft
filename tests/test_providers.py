0fa66321fcda9c4c42af7af80c6c9871567f3c5d
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2021-07-22 15:32:54 -0300

    providers: install sudo in base setup
    Make sudo available in the base image. This can be removed once
    craft-parts is changed to invoke sudo only if necessary.
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -17,6 +17,7 @@
 import os
 import pathlib
 import re
+import subprocess
 from unittest import mock
 from unittest.mock import call
 
@@ -120,16 +121,51 @@ def clear_environment(monkeypatch):
 @pytest.mark.parametrize(
     "alias", [bases.BuilddBaseAlias.BIONIC, bases.BuilddBaseAlias.FOCAL]
 )
-def test_base_configuration_setup(mock_inject, monkeypatch, alias):
+def test_base_configuration_setup(mock_executor, mock_inject, monkeypatch, alias):
 
     config = providers.CharmcraftBuilddBaseConfiguration(alias=alias)
     config.setup(executor=mock_executor)
+
+    assert mock_executor.mock_calls == [
+        call.execute_run(
+            [
+                "apt-get",
+                "install",
+                "-y",
+                "sudo",
+            ],
+            check=True,
+            capture_output=True,
+        ),
+    ]
+
 
     assert mock_inject.mock_calls == [
         call(executor=mock_executor, snap_name="charmcraft", classic=True)
     ]
 
     assert config.compatibility_tag == "charmcraft-buildd-base-v0.0"
+
+
+def test_base_configuration_setup_apt_error(mock_executor):
+    alias = bases.BuilddBaseAlias.FOCAL
+    apt_cmd = ["apt-get", "install", "-y", "sudo"]
+    mock_executor.execute_run.side_effect = subprocess.CalledProcessError(
+        -1,
+        apt_cmd,
+        "some output",
+        "some error",
+    )
+
+    config = providers.CharmcraftBuilddBaseConfiguration(alias=alias)
+
+    with pytest.raises(
+        bases.BaseConfigurationError,
+        match=r"Failed to install the required dependencies.",
+    ) as exc_info:
+        config.setup(executor=mock_executor)
+
+    assert exc_info.value.__cause__ is not None
 
 
 def test_base_configuration_setup_snap_injection_error(mock_executor, mock_inject):

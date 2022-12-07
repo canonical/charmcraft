9f5f0692edde023180cee4dd9c5b2f48632dfb5d
Alex Lowe <alex.lowe@canonical.com>
2022-12-07 16:32:15 -0500

    Prepare for merge to canonical/charmcraft

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  branchPrefix: "renovate/",
-  dryRun: null,
+  branchPrefix: "renovate-",
   username: "renovate-release",
   gitAuthor: "Renovate Bot <bot@renovateapp.com>",
   onboarding: true,
   platform: "github",
-  includeForks: true,
-  repositories: ["lengau/charmcraft"],
+  repositories: ["canonical/charmcraft"],
 }

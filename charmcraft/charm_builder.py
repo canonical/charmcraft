25d18cbf6a162ea3afd405286ab76de832c94d74
Carl Csaposs <carl.csaposs@canonical.com>
2023-05-18 18:16:32 +0000

    Remove existing symlink if present

--- charmcraft/charm_builder.py
+++ charmcraft/charm_builder.py
@@ -39,7 +39,7 @@ from charmcraft.utils import make_executable
 # Some constants that are used through the code.
 WORK_DIRNAME = "work_dir"
 VENV_DIRNAME = "venv"
-SITE_PACKAGES_DIRNAME = "site-packages"
+SITE_PACKAGES_SYMLINK_NAME = "site-packages"
 STAGING_VENV_DIRNAME = "staging-venv"
 DEPENDENCIES_HASH_FILENAME = "charmcraft-dependencies-hash.txt"
 
@@ -326,7 +326,9 @@ class CharmBuilder:
         # symlink /site-packages directory in charm to virtual environment site-packages directory
         # (backwards compatability for charms that do not use the full virtual environment)
         site_packages_dir = _find_venv_site_packages(venv_dir)
-        (self.installdir / SITE_PACKAGES_DIRNAME).symlink_to(site_packages_dir)
+        site_packages_symlink_path = self.installdir / SITE_PACKAGES_SYMLINK_NAME
+        site_packages_symlink_path.unlink(missing_ok=True)
+        site_packages_symlink_path.symlink_to(site_packages_dir)
 
 
 def _find_venv_bin(basedir, exec_base):

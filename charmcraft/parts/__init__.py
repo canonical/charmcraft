b237b429feca416006410d61311d6dc09ae3c49d
Alex Lowe <alex.lowe@canonical.com>
2023-10-17 15:39:25 -0400

    refactor(parts): lifecycle manager to its own file

--- charmcraft/parts/__init__.py
+++ charmcraft/parts/__init__.py
@@ -16,20 +16,14 @@
 
 """Craft-parts setup, lifecycle and plugins."""
 
-import os
-import pathlib
-import shlex
-from typing import Any, Dict, List
+from typing import Any, Dict
 
-from craft_cli import CraftError, emit
-from craft_parts import LifecycleManager, Step, plugins
-from craft_parts.errors import PartsError
+from craft_parts import plugins
 from craft_parts.parts import PartSpec
-from xdg import BaseDirectory  # type: ignore
 
-from charmcraft import charm_builder, instrum
 from charmcraft.parts.bundle import BundlePlugin
 from charmcraft.parts.charm import CharmPlugin, CharmPluginProperties
+from charmcraft.parts.lifecycle import PartsLifecycle
 from charmcraft.reactive_plugin import ReactivePlugin, ReactivePluginProperties
 
 __all__ = [
@@ -83,111 +77,3 @@ def process_part_config(data: Dict[str, Any]) -> Dict[str, Any]:
     full_config.update(data)
 
     return full_config
-
-
-class PartsLifecycle:
-    """Create and manage the parts lifecycle.
-
-    :param all_parts: A dictionary containing the parts defined in the project.
-    :param work_dir: The working directory for parts processing.
-    :param project_dir: The directory containing the charm project.
-    :param ignore_local_sources: A list of local source patterns to be ignored.
-    :param name: Charm name as defined in ``metadata.yaml``.
-    """
-
-    def __init__(
-        self,
-        all_parts: Dict[str, Any],
-        *,
-        work_dir: pathlib.Path,
-        project_dir: pathlib.Path,
-        project_name: str,
-        ignore_local_sources: List[str],
-    ):
-        self._all_parts = all_parts.copy()
-        self._project_dir = project_dir
-
-        # set the cache dir for parts package management
-        cache_dir = BaseDirectory.save_cache_path("charmcraft")
-
-        try:
-            self._lcm = LifecycleManager(
-                {"parts": all_parts},
-                application_name="charmcraft",
-                work_dir=work_dir,
-                cache_dir=cache_dir,
-                ignore_local_sources=ignore_local_sources,
-                project_name=project_name,
-            )
-        except PartsError as err:
-            raise CraftError(f"Error bootstrapping lifecycle manager: {err}") from err
-
-    @property
-    def prime_dir(self) -> pathlib.Path:
-        """Return the parts prime directory path."""
-        return self._lcm.project_info.prime_dir
-
-    def run(self, target_step: Step) -> None:
-        """Run the parts lifecycle.
-
-        :param target_step: The final step to execute.
-
-        :raises CraftError: On error during lifecycle ops.
-        :raises RuntimeError: On unexpected error.
-        """
-        previous_dir = os.getcwd()
-        try:
-            os.chdir(self._project_dir)
-
-            # invalidate build if packing a charm and entrypoint changed
-            if "charm" in self._all_parts:
-                charm_part = self._all_parts["charm"]
-                if charm_part.get("plugin") == "charm":
-                    entrypoint = os.path.normpath(charm_part["charm-entrypoint"])
-                    dis_entrypoint = os.path.normpath(_get_dispatch_entrypoint(self.prime_dir))
-                    if entrypoint != dis_entrypoint:
-                        self._lcm.clean(Step.BUILD, part_names=["charm"])
-                        self._lcm.reload_state()
-
-            emit.debug(f"Executing parts lifecycle in {str(self._project_dir)!r}")
-            actions = self._lcm.plan(target_step)
-            emit.debug(f"Parts actions: {actions}")
-            with instrum.Timer("Running action executor") as executor_timer:
-                with self._lcm.action_executor() as aex:
-                    executor_timer.mark("Context enter")
-                    for act in actions:
-                        emit.progress(f"Running step {act.step.name} for part {act.part_name!r}")
-                        with instrum.Timer("Running step", step=act.step.name, part=act.part_name):  # type: ignore[arg-type]
-                            with emit.open_stream("Execute action") as stream:
-                                aex.execute([act], stdout=stream, stderr=stream)
-                    executor_timer.mark("Context exit")
-
-        except RuntimeError as err:
-            raise RuntimeError(f"Parts processing internal error: {err}") from err
-        except OSError as err:
-            msg = err.strerror
-            if err.filename:
-                msg = f"{err.filename}: {msg}"
-            raise CraftError(f"Parts processing error: {msg}") from err
-        except Exception as err:
-            raise CraftError(f"Parts processing error: {err}") from err
-        finally:
-            os.chdir(previous_dir)
-
-
-def _get_dispatch_entrypoint(dirname: pathlib.Path) -> str:
-    """Read the entrypoint from the dispatch file."""
-    dispatch = dirname / charm_builder.DISPATCH_FILENAME
-    entrypoint_str = ""
-    try:
-        with dispatch.open("rt", encoding="utf8") as fh:
-            last_line = None
-            for line in fh:
-                if line.strip():
-                    last_line = line
-            if last_line:
-                entrypoint_str = shlex.split(last_line)[-1]
-    except (OSError, UnicodeDecodeError):
-        return ""
-
-    return entrypoint_str
---
+++ tests/unit/parts/test_lifecycle.py
@@ -0,0 +1,199 @@
+# Copyright 2023 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For further info, check https://github.com/canonical/charmcraft
+import pathlib
+from unittest.mock import ANY, call, patch
+
+import pytest
+from craft_cli import CraftError
+from craft_parts import Action, ActionType, PartsError, Step
+
+from charmcraft.parts import lifecycle
+
+
+def test_partslifecycle_bad_bootstrap(tmp_path):
+    fake_error = PartsError("pumba")
+    with patch("craft_parts.LifecycleManager.__init__") as mock:
+        mock.side_effect = fake_error
+        with pytest.raises(CraftError) as cm:
+            lifecycle.PartsLifecycle(
+                all_parts={},
+                work_dir=pathlib.Path("/some/workdir"),
+                project_dir=tmp_path,
+                project_name="test",
+                ignore_local_sources=["*.charm"],
+            )
+        exc = cm.value
+        assert str(exc) == "Error bootstrapping lifecycle manager: pumba"
+        assert exc.__cause__ == fake_error
+
+
+def test_partslifecycle_prime_dir(tmp_path):
+    data = {
+        "plugin": "charm",
+        "source": ".",
+    }
+
+    with patch("craft_parts.LifecycleManager.refresh_packages_list"):
+        lc = lifecycle.PartsLifecycle(
+            all_parts={"charm": data},
+            work_dir=pathlib.Path("/some/workdir"),
+            project_dir=tmp_path,
+            project_name="test",
+            ignore_local_sources=["*.charm"],
+        )
+    assert lc.prime_dir == pathlib.Path("/some/workdir/prime")
+
+
+def test_partslifecycle_run_new_entrypoint(tmp_path, monkeypatch):
+    data = {
+        "plugin": "charm",
+        "source": ".",
+        "charm-entrypoint": "my-entrypoint",
+        "charm-python-packages": ["pkg1", "pkg2"],
+        "charm-requirements": [],
+    }
+
+    # create dispatcher from previous run
+    prime_dir = tmp_path / "prime"
+    prime_dir.mkdir()
+    dispatch = prime_dir / "dispatch"
+    dispatch.write_text(
+        'JUJU_DISPATCH_PATH="${JUJU_DISPATCH_PATH:-$0}" PYTHONPATH=lib:venv ./src/charm.py'
+    )
+
+    lc = lifecycle.PartsLifecycle(
+        all_parts={"charm": data},
+        work_dir=tmp_path,
+        project_dir=tmp_path,
+        project_name="test",
+        ignore_local_sources=["*.charm"],
+    )
+
+    with patch("craft_parts.LifecycleManager.clean") as mock_clean:
+        with patch("craft_parts.LifecycleManager.plan") as mock_plan:
+            mock_plan.side_effect = SystemExit("test")
+            with pytest.raises(SystemExit, match="test"):
+                lc.run(Step.PRIME)
+
+    mock_clean.assert_called_once_with(Step.BUILD, part_names=["charm"])
+
+
+def test_partslifecycle_run_same_entrypoint(tmp_path, monkeypatch):
+    data = {
+        "plugin": "charm",
+        "source": ".",
+        "charm-entrypoint": "src/charm.py",
+        "charm-python-packages": ["pkg1", "pkg2"],
+        "charm-requirements": [],
+    }
+
+    # create dispatcher from previous run
+    prime_dir = tmp_path / "prime"
+    prime_dir.mkdir()
+    dispatch = prime_dir / "dispatch"
+    dispatch.write_text(
+        'JUJU_DISPATCH_PATH="${JUJU_DISPATCH_PATH:-$0}" PYTHONPATH=lib:venv ./src/charm.py'
+    )
+
+    lc = lifecycle.PartsLifecycle(
+        all_parts={"charm": data},
+        work_dir=tmp_path,
+        project_dir=tmp_path,
+        project_name="test",
+        ignore_local_sources=["*.charm"],
+    )
+
+    with patch("craft_parts.LifecycleManager.clean") as mock_clean:
+        with patch("craft_parts.LifecycleManager.plan") as mock_plan:
+            mock_plan.side_effect = SystemExit("test")
+            with pytest.raises(SystemExit, match="test"):
+                lc.run(Step.PRIME)
+
+    mock_clean.assert_not_called()
+
+
+def test_partslifecycle_run_no_previous_entrypoint(tmp_path, monkeypatch):
+    data = {
+        "plugin": "charm",
+        "source": ".",
+        "charm-entrypoint": "my-entrypoint",
+        "charm-python-packages": ["pkg1", "pkg2"],
+        "charm-requirements": [],
+    }
+
+    lc = lifecycle.PartsLifecycle(
+        all_parts={"charm": data},
+        work_dir=tmp_path,
+        project_dir=tmp_path,
+        project_name="test",
+        ignore_local_sources=["*.charm"],
+    )
+
+    with patch("craft_parts.LifecycleManager.clean") as mock_clean:
+        with patch("craft_parts.LifecycleManager.plan") as mock_plan:
+            mock_plan.side_effect = SystemExit("test")
+            with pytest.raises(SystemExit, match="test"):
+                lc.run(Step.PRIME)
+
+    mock_clean.assert_called_once_with(Step.BUILD, part_names=["charm"])
+
+
+def test_partslifecycle_run_actions_progress(tmp_path, monkeypatch, emitter):
+    data = {
+        "plugin": "nil",
+        "source": ".",
+    }
+
+    lc = lifecycle.PartsLifecycle(
+        all_parts={"testpart": data},
+        work_dir=tmp_path,
+        project_dir=tmp_path,
+        project_name="test",
+        ignore_local_sources=[],
+    )
+
+    action1 = Action(
+        part_name="testpart", step=Step.STAGE, action_type=ActionType.RUN, reason=None
+    )
+    action2 = Action(
+        part_name="testpart", step=Step.PRIME, action_type=ActionType.RUN, reason=None
+    )
+
+    with patch("craft_parts.LifecycleManager.plan") as mock_plan:
+        mock_plan.return_value = [action1, action2]
+        with patch("craft_parts.executor.executor.ExecutionContext.execute") as mock_exec:
+            lc.run(Step.PRIME)
+
+    emitter.assert_progress("Running step STAGE for part 'testpart'")
+    emitter.assert_progress("Running step PRIME for part 'testpart'")
+    assert mock_exec.call_args_list == [
+        call([action1], stdout=ANY, stderr=ANY),
+        call([action2], stdout=ANY, stderr=ANY),
+    ]
+
+
+def test_parthelpers_get_dispatch_entrypoint(tmp_path):
+    dispatch = tmp_path / "dispatch"
+    dispatch.write_text(
+        'JUJU_DISPATCH_PATH="${JUJU_DISPATCH_PATH:-$0}" PYTHONPATH=lib:venv ./my/entrypoint'
+    )
+    entrypoint = lifecycle._get_dispatch_entrypoint(tmp_path)
+    assert entrypoint == "./my/entrypoint"
+
+
+def test_parthelpers_get_dispatch_entrypoint_no_file(tmp_path):
+    entrypoint = lifecycle._get_dispatch_entrypoint(tmp_path)
+    assert entrypoint == ""

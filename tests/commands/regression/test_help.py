4f5e3c4e2d1b381f706788cff5d2853fdf929f9a
Alex Lowe <alex.lowe@canonical.com>
2023-10-27 14:21:17 -0400

    fix(command): backwards-compatible version command

--- tests/commands/regression/test_help.py
+++ tests/commands/regression/test_help.py
@@ -20,6 +20,28 @@ import pytest
 import pytest_check
 
 
+# Copyright 2023 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For further info, check https://github.com/canonical/charmcraft
+import subprocess
+import sys
+
+import pytest
+import pytest_check
+
+
 @pytest.mark.parametrize(
     "command",
     [
@@ -50,11 +72,10 @@ import pytest_check
     ],
 )
 def test_compare_command_and_legacy_options(command):
-    command_help = subprocess.run(
+    help_cmd = subprocess.Popen(
         [sys.executable, "-m", "charmcraft", command, "-h"],
         text=True,
-        capture_output=True,
-        check=True,
+        stderr=subprocess.PIPE
     )
     legacy_help = subprocess.run(
         [sys.executable, "-m", "charmcraft.main", command, "-h"],
@@ -62,13 +83,21 @@ def test_compare_command_and_legacy_options(command):
         capture_output=True,
         check=True,
     )
+    help_cmd.wait()
+    command_help = help_cmd.stderr.read()
 
     past_options = False
-    for line in command_help.stderr:
+    for line in legacy_help.stderr.splitlines():
         if not past_options:
-            if line == "Options:\n":
+            if line.strip() == "Options:":
                 past_options = True
             continue
-        if line == "See also:\n":
+        if line.strip() == "See also:":
             break
-        pytest_check.is_in(line, legacy_help)
+
+        if ":" not in line:
+            continue
+        option = line.strip().split(":", maxsplit=1)[0]
+
+        pytest_check.is_in(option, command_help, msg=f"Missing: {option}")
+

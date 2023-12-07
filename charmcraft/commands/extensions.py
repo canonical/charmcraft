dec4d61b496bcea862173937c51e8f79743d9ff7
Alex Lowe <alex.lowe@canonical.com>
2023-12-07 12:45:27 -0500

    fix: expand-extensions overview (#1416)
    This PR targets the hotfix branch to cover the legacy command.
    
    Fixes #1411

--- charmcraft/commands/extensions.py
+++ charmcraft/commands/extensions.py
@@ -83,8 +83,11 @@ class ExpandExtensionsCommand(BaseCommand):
     help_msg = "Expand extensions in charmcraft.yaml"
     overview = dedent(
         """
-        Extensions listed charmcraft.yaml will be
-        expanded and shown as output.
+        Expand charmcraft.yaml using the extensions specified in the file and
+        output the resulting configuration to the terminal.
+
+        This allows you to see how the extensions used modify your existing
+        charmcraft.yaml file.
         """
     )
 

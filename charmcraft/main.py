a4218b2be093681ebd710e34dbc77d3aa38f73d3
Alex Lowe <alex.lowe@canonical.com>
2024-02-22 12:33:48 -0500

    chore(merge): fix merge conflicts

--- charmcraft/main.py
+++ charmcraft/main.py
@@ -33,7 +33,7 @@ from craft_cli import (
 )
 
 from charmcraft import config, const, env, utils
-from charmcraft.commands import clean, extensions, init, pack, store, version
+from charmcraft.commands import clean, extensions, pack, store, version
 from charmcraft.parts import setup_parts
 
 # set up all the libs' loggers in DEBUG level so their content is grabbed by craft-cli's Emitter
@@ -58,7 +58,6 @@ See https://charmhub.io/publishing for more information.
 # important when listing commands and showing help.
 _basic_commands = [
     clean.CleanCommand,
-    init.InitCommand,
     pack.PackCommand,
     version.VersionCommand,
 ]

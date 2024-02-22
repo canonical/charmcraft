5e3b927c04e6f46817b37c0ab02143de9a7aa4de
Alex Lowe <alex.lowe@canonical.com>
2024-02-22 10:33:05 -0500

    chore: replace most lifecycle commands with upstream ones (#1538)
    Work towards #1532
    
    After this we still need to extend the pack command from upstream.

--- charmcraft/application/commands/__init__.py
+++ charmcraft/application/commands/__init__.py
@@ -25,12 +25,7 @@ from charmcraft.application.commands.extensions import (
 from charmcraft.application.commands.init import InitCommand
 from charmcraft.application.commands.lifecycle import (
     get_lifecycle_commands,
-    BuildCommand,
-    CleanCommand,
     PackCommand,
-    PullCommand,
-    PrimeCommand,
-    StageCommand,
 )
 from charmcraft.application.commands.store import (
     # auth
@@ -130,12 +125,7 @@ __all__ = [
     "ExtensionsCommand",
     "InitCommand",
     "get_lifecycle_commands",
-    "BuildCommand",
-    "CleanCommand",
     "PackCommand",
-    "PrimeCommand",
-    "PullCommand",
-    "StageCommand",
     "LoginCommand",
     "LogoutCommand",
     "WhoamiCommand",

ee5984be6b8673f6790b7d30a9a7a7befb67f1a5
Alex Lowe <alex.lowe@canonical.com>
2024-03-05 16:15:55 -0500

    refactor: remove unused extension command implementations (#1572)
    Moves the tests to the craft-application implementations too.

--- charmcraft/main.py
+++ charmcraft/main.py
@@ -34,7 +34,7 @@ from craft_cli import (
 )
 
 from charmcraft import config, const, env, utils
-from charmcraft.commands import extensions, pack, store, version
+from charmcraft.commands import pack, store, version
 from charmcraft.parts import setup_parts
 
 # set up all the libs' loggers in DEBUG level so their content is grabbed by craft-cli's Emitter
@@ -88,15 +88,9 @@ _charmhub_commands = [
     store.ListResourcesCommand,
     store.UploadResourceCommand,
 ]
-_extensions_commands = [
-    extensions.ExtensionsCommand,
-    extensions.ListExtensionsCommand,
-    extensions.ExpandExtensionsCommand,
-]
 COMMAND_GROUPS = [
     CommandGroup("Basic", _basic_commands),
     CommandGroup("Charmhub", _charmhub_commands),
-    CommandGroup("Extensions", _extensions_commands),
 ]
 
 # non-charmcraft useful environment variables to log

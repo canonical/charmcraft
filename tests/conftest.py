9247b22516ff21593181cee1024f472143881d6d
Alex Lowe <alex.lowe@canonical.com>
2024-06-11 15:14:10 -0400

    test: add unit test for pack command invalid args

--- tests/conftest.py
+++ tests/conftest.py
@@ -66,13 +66,20 @@ def mock_store_client():
 
 @pytest.fixture()
 def service_factory(
-    fs, fake_project_dir, fake_prime_dir, simple_charm, mock_store_client
+    fs, fake_project_dir, fake_prime_dir, simple_charm, mock_store_client, default_build_plan
 ) -> services.CharmcraftServiceFactory:
     factory = services.CharmcraftServiceFactory(app=APP_METADATA)
 
     factory.set_kwargs(
         "package",
         project_dir=fake_project_dir,
+        build_plan=default_build_plan,
+    )
+    factory.set_kwargs(
+        "lifecycle",
+        work_dir=pathlib.Path("/project"),
+        cache_dir=pathlib.Path("/cache"),
+        build_plan=default_build_plan,
     )
 
     factory.project = simple_charm

c3b69774a4711d8c9cdf4d0c6344c35b428a15e0
Alex Lowe <alex.lowe@canonical.com>
2024-07-18 13:10:37 -0400

    fix(builder)!: in strict mode, don't install deps (#1728)
    Strict install requires all dependencies to be included in requirements
    
    CI issue is fixed in #1734

--- charmcraft/models/project.py
+++ charmcraft/models/project.py
@@ -14,6 +14,7 @@
 #
 # For further info, check https://github.com/canonical/charmcraft
 """Project-related models for Charmcraft."""
+
 import abc
 import datetime
 import pathlib
@@ -406,7 +407,10 @@ class CharmcraftBuildPlanner(models.BuildPlanner):
                     )
                 build_infos.append(
                     models.BuildInfo(
-                        platform_name, build_on=platform_name, build_for=platform_name, base=base
+                        platform_name,
+                        build_on=platform_name,
+                        build_for=platform_name,
+                        base=base,
                     )
                 )
             else:
@@ -776,7 +780,8 @@ class BasesCharm(CharmcraftProject):
         ],
     )
     extra_bindings: dict[str, Any] | None = pydantic.Field(
-        default=None, description="A key-only mapping representing extra bindings needed."
+        default=None,
+        description="A key-only mapping representing extra bindings needed.",
     )
     peers: dict[str, Any] | None = pydantic.Field(
         default=None,

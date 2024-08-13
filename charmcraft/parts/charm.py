0d3b27136fb9c21e6d7c091dc4ca4ac6bfee3aca
Alex Lowe <alex.lowe@canonical.com>
2024-08-13 13:19:20 -0400

    fix(charm-plugin): remove existence checks for reqs (#1808)
    Requirements files might not exist yet when the charm plugin is
    instantiated, so don't check for their existence in the plugin.

--- charmcraft/parts/charm.py
+++ charmcraft/parts/charm.py
@@ -30,11 +30,6 @@ from craft_parts.packages import platform
 from craft_parts.utils import os_utils
 
 from charmcraft import charm_builder, env, instrum
-from charmcraft.errors import DependencyError
-from charmcraft.utils import (
-    get_requirements_file_package_names,
-    validate_strict_dependencies,
-)
 
 PACKAGE_NAME_REGEX = re.compile(r"[A-Za-z0-9_.-]+")
 
@@ -91,12 +86,6 @@ class CharmPluginProperties(plugins.PluginProperties, plugins.PluginModel):
             )
         project_dirpath = pathlib.Path(values["source"])
 
-        # check that all indicated files are present
-        for reqs_filename in charm_requirements:
-            reqs_path = project_dirpath / reqs_filename
-            if not reqs_path.is_file():
-                raise ValueError(f"requirements file {str(reqs_path)!r} not found")
-
         # if nothing indicated, and default file is there, use it
         default_reqs_name = "requirements.txt"
         if not charm_requirements and (project_dirpath / default_reqs_name).is_file():
@@ -125,30 +114,6 @@ class CharmPluginProperties(plugins.PluginProperties, plugins.PluginModel):
             raise ValueError(
                 "'charm-strict-dependencies' requires at least one requirements file."
             )
-
-        invalid_binaries = set()
-        for binary_package in values.get("charm_binary_python_packages", []):
-            if not PACKAGE_NAME_REGEX.fullmatch(binary_package):
-                invalid_binaries.add(binary_package)
-
-        if invalid_binaries:
-            raise ValueError(
-                "'charm-binary-python-packages' may contain only package names allowed "
-                "to be installed from binary if 'charm-strict-dependencies' is enabled. "
-                f"Invalid package names: {sorted(invalid_binaries)}"
-            )
-
-        try:
-            validate_strict_dependencies(
-                get_requirements_file_package_names(
-                    *(pathlib.Path(r) for r in values["charm_requirements"])
-                ),
-                values.get("charm_binary_python_packages", []),
-            )
-        except DependencyError as e:
-            raise ValueError(
-                "All dependencies must be specified in requirements files for strict dependencies."
-            ) from e
 
         return charm_strict_dependencies
 
--- tests/unit/test_parts.py
+++ tests/unit/test_parts.py
@@ -70,16 +70,6 @@ def test_partconfig_strict_dependencies_success(fs: FakeFilesystem, part_config,
             {"charm-requirements": ["req.txt"], "charm-python-packages": ["ops"]},
             "'charm-python-packages' must not be set if 'charm-strict-dependencies' is enabled",
         ),
-        (
-            {"charm-requirements": ["req.txt"], "charm-binary-python-packages": ["not-here"]},
-            "All dependencies must be specified in requirements files for strict dependencies.",
-        ),
-        (
-            {"charm-requirements": ["req.txt"], "charm-binary-python-packages": ["ops>=2.6"]},
-            "'charm-binary-python-packages' may contain only package names allowed to be "
-            "installed from binary if 'charm-strict-dependencies' is enabled. Invalid "
-            "package names: ['ops>=2.6']",
-        ),
         ({}, "'charm-strict-dependencies' requires at least one requirements file."),
     ],
 )

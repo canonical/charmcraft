5912e4527c451aef49c5baa1ccadf876bc6a7d89
Alex Lowe <alex.lowe@canonical.com>
2024-09-09 16:55:40 -0400

    ugh Windows

--- tests/integration/services/test_image.py
+++ tests/integration/services/test_image.py
@@ -15,6 +15,8 @@
 # For further info, check https://github.com/canonical/charmcraft
 """Integration tests for the Image service."""
 
+import sys
+
 import pytest
 
 from charmcraft import application, services
@@ -39,5 +41,6 @@ def image_service() -> services.ImageService:
         "docker://quay.io/prometheus/blackbox-exporter:v0.24.0@sha256:3af31f8bd1ad2907b4b0f7c485fde3de0a8ee0b498d42fc971f0698885c03acb",
     ],
 )
+@pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
 def test_get_maybe_id_from_docker_no_exceptions(image_service: services.ImageService, url):
     image_service.get_maybe_id_from_docker(url)

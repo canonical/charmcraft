ec881a4657888c593b35e99adc42514d29dbdfc5
javierdelapuente <javier.delapuente@canonical.com>
2024-09-17 15:20:03 +0200

    feat: fastapi-framework init profile and extension (#1868)
    This PR implements the spec ISD160 - 12-Factor FastAPI Support" for
    charmcraft.
    
    For that, a new init profile called "fastapi-framework" and a new
    fastapi extension, called "fastapi-framework" are needed. These
    additions are similar to the flask and django init profiles and
    extensions.
    
    For the fastapi profile, the base 24.04 is used.
    
    ---------
    
    Co-authored-by: Alex Lowe <alex.lowe@canonical.com>

--- tests/extensions/test_app.py
+++ tests/extensions/test_app.py
@@ -19,6 +19,7 @@ from charmcraft.errors import ExtensionError
 from charmcraft.extensions import apply_extensions
 from charmcraft.extensions.app import (
     DjangoFramework,
+    FastAPIFramework,
     FlaskFramework,
     GoFramework,
 )
@@ -178,14 +179,20 @@ def flask_input_yaml_fixture():
                 "name": "test-go",
                 "summary": "test summary",
                 "description": "test description",
-                "bases": [{"name": "ubuntu", "channel": "24.04"}],
+                "base": "ubuntu@24.04",
+                "platforms": {
+                    "amd64": None,
+                },
                 "extensions": ["go-framework"],
             },
             True,
             {
                 "actions": GoFramework.actions,
                 "assumes": ["k8s-api"],
-                "bases": [{"channel": "24.04", "name": "ubuntu"}],
+                "base": "ubuntu@24.04",
+                "platforms": {
+                    "amd64": None,
+                },
                 "containers": {
                     "app": {"resource": "app-image"},
                 },
@@ -225,6 +232,72 @@ def flask_input_yaml_fixture():
                 "resources": {
                     "app-image": {
                         "description": "go application image.",
+                        "type": "oci-image",
+                    },
+                },
+                "summary": "test summary",
+                "type": "charm",
+            },
+        ),
+        (
+            {
+                "type": "charm",
+                "name": "test-fastapi",
+                "summary": "test summary",
+                "description": "test description",
+                "base": "ubuntu@24.04",
+                "platforms": {
+                    "amd64": None,
+                },
+                "extensions": ["fastapi-framework"],
+            },
+            True,
+            {
+                "actions": FastAPIFramework.actions,
+                "assumes": ["k8s-api"],
+                "base": "ubuntu@24.04",
+                "platforms": {
+                    "amd64": None,
+                },
+                "containers": {
+                    "app": {"resource": "app-image"},
+                },
+                "description": "test description",
+                "name": "test-fastapi",
+                "charm-libs": [
+                    {"lib": "traefik_k8s.ingress", "version": "2"},
+                    {"lib": "observability_libs.juju_topology", "version": "0"},
+                    {"lib": "grafana_k8s.grafana_dashboard", "version": "0"},
+                    {"lib": "loki_k8s.loki_push_api", "version": "0"},
+                    {"lib": "data_platform_libs.data_interfaces", "version": "0"},
+                    {"lib": "prometheus_k8s.prometheus_scrape", "version": "0"},
+                    {"lib": "redis_k8s.redis", "version": "0"},
+                    {"lib": "data_platform_libs.s3", "version": "0"},
+                    {"lib": "saml_integrator.saml", "version": "0"},
+                ],
+                "config": {
+                    "options": {**FastAPIFramework.options},
+                },
+                "parts": {
+                    "charm": {
+                        "plugin": "charm",
+                        "source": ".",
+                        "build-snaps": ["rustup"],
+                        "override-build": "rustup default stable\ncraftctl default",
+                    }
+                },
+                "peers": {"secret-storage": {"interface": "secret-storage"}},
+                "provides": {
+                    "metrics-endpoint": {"interface": "prometheus_scrape"},
+                    "grafana-dashboard": {"interface": "grafana_dashboard"},
+                },
+                "requires": {
+                    "logging": {"interface": "loki_push_api"},
+                    "ingress": {"interface": "ingress", "limit": 1},
+                },
+                "resources": {
+                    "app-image": {
+                        "description": "fastapi application image.",
                         "type": "oci-image",
                     },
                 },

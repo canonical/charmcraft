af2fc63545108ac49fbb6bc4a2110adbedc98a8c
Erin Conley <erin.conley@canonical.com>
2024-10-30 09:53:53 +0100

    Create greeting_app.py

---
+++ docs/tutorial/code/flask/greeting_app.py
@@ -0,0 +1,14 @@
+import flask
+
+app = flask.Flask(__name__)
+app.config.from_prefixed_env()
+
+
+@app.route("/")
+def index():
+    greeting = app.config.get("GREETING", "Hello, world!")
+    return f"{greeting}\n"
+
+
+if __name__ == "__main__":
+    app.run()

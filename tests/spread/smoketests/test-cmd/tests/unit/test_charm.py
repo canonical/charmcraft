108c4eae0ebdcd367d1e2ceadfbaf35c2ce0892b
Alex Lowe <alex.lowe@canonical.com>
2025-07-23 11:55:57 -0400

    test: add `charmcraft test` spread test

---
+++ tests/spread/smoketests/test-cmd/tests/unit/test_charm.py
@@ -0,0 +1,26 @@
+# Copyright 2025 Alex Lowe
+# See LICENSE file for licensing details.
+#
+# To learn more about testing, see https://ops.readthedocs.io/en/latest/explanation/testing.html
+
+import pytest
+from ops import testing
+
+from charm import TestCmdCharm
+
+
+def mock_get_version():
+    """Get a mock version string without executing the workload code."""
+    return "1.0.0"
+
+
+def test_start(monkeypatch: pytest.MonkeyPatch):
+    """Test that the charm has the correct state after handling the start event."""
+    # Arrange:
+    ctx = testing.Context(TestCmdCharm)
+    monkeypatch.setattr("charm.test_cmd.get_version", mock_get_version)
+    # Act:
+    state_out = ctx.run(ctx.on.start(), testing.State())
+    # Assert:
+    assert state_out.workload_version is not None
+    assert state_out.unit_status == testing.ActiveStatus()

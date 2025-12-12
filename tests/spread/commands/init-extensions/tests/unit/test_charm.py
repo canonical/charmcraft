c90a575d866433dec3c7f2605750986fa3393629
James Garner <james.garner@canonical.com>
2025-12-12 15:43:44 +1300

    test: modernise test

--- tests/spread/commands/init-extensions/tests/unit/test_charm.py
+++ tests/spread/commands/init-extensions/tests/unit/test_charm.py
@@ -7,17 +7,14 @@ import os
 import pathlib
 
 import charm
-import scenario
-import scenario.errors
+from ops import testing
 
 
 def test_smoke():
     """The purpose of this test is that the charm does not raise on a handled event."""
     os.chdir(pathlib.Path(charm.__file__).parent.parent)
-    ctx = scenario.Context(charm.HelloWorldCharm)
-    state_in = scenario.State.from_context(ctx)
-    out = ctx.run(
-        ctx.on.pebble_ready(next(iter(state_in.containers))),
-        state_in,
-    )
-    assert type(out.unit_status) in (scenario.WaitingStatus, scenario.BlockedStatus)
+    ctx = testing.Context(charm.HelloWorldCharm)
+    state_in = testing.State.from_context(ctx)
+    container = next(iter(state_in.containers))
+    state_out = ctx.run(ctx.on.pebble_ready(container), state_in)
+    assert type(state_out.unit_status) in (testing.WaitingStatus, testing.BlockedStatus)

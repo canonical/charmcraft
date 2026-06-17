85af1b34b42646b148df12968fe5fcb3bfa75b5d
jahn-junior <jj.coldiron@canonical.com>
2026-06-17 14:46:41 -0700

    docs: use more robust versioning

--- docs/conf.py
+++ docs/conf.py
@@ -28,10 +28,14 @@ import charmcraft
 project = "Charmcraft"
 author = "Canonical"
 
-# The commit hash in the dev release version confuses the spellchecker
-release = os.environ.get("READTHEDOCS_VERSION", charmcraft.__version__)
-if ".post" in release:
-    release = "dev"
+# Version string in sidebar
+if os.environ.get("READTHEDOCS_VERSION_TYPE", "external") != "external":
+    rtd_version = os.environ.get("READTHEDOCS_VERSION", "latest")
+    release = rtd_version if rtd_version != "latest" else "dev"
+else:
+    # Because of Autotools, we can safely assume the version starts with `n.n`
+    major, minor, *_ = charmcraft.__version__.split(".")
+    release = f"{major}.{minor}"
 
 html_title = project + " documentation"
 
